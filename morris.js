48bc0bfdc3788c0bebe5bdd8013d6083ccd100c8
Charly Koza <charly.koza@gmail.com>
2013-07-05 16:53:05 +0200

    morris now compatible amd

--- morris.js
+++ morris.js
@@ -6,7 +6,12 @@
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
-  Morris = window.Morris = {};
+  Morris = {};
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Morris;
+  } else {
+    window.Morris = Morris
+  }
 
   $ = jQuery;
 
