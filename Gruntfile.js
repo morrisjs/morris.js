19fb3ba5122cae95d0adf97b840b00e0c26f5a72
sudodoki <smd.deluzion@gmail.com>
2013-09-16 20:28:02 +0300

    update Grunt version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,20 @@
 module.exports = function (grunt) {
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
   grunt.initConfig({
     coffee: {
       lib: {
-        src: ['build/morris.coffee'],
-        dest: '.',
-        options: { bare: false }
+        options: { bare: false },
+        files: {
+          'morris.js': ['build/morris.coffee']
+        }
       },
       spec: {
-        src: ['build/spec.coffee'],
-        dest: 'build',
-        options: { bare: true }
-      }
+        options: { bare: true },
+        files: {
+          'build/spec.js': ['build/spec.coffee']
+        }
+      },
     },
     concat: {
       'build/morris.coffee': [
@@ -33,24 +37,26 @@ module.exports = function (grunt) {
         }
       }
     },
-    min: {
-      'morris.min.js': 'morris.js'
-    },
-    mocha: {
-      spec: {
-        src: 'spec/specs.html',
-        run: true
+    uglify: {
+      build: {
+        files: {
+          'morris.min.js': 'morris.js'
+        }
       }
     },
-    watch: {
+    mocha: {
+      index: ['spec/specs.html'],
+      options: {run: true}
+    },
+    npm: {
       files: ['lib/**/*.coffee', 'spec/lib/**/*.coffee', 'spec/support/**/*.coffee', 'less/**/*.less'],
       tasks: 'default'
+    },
+    watch: {
+      files:  'lib/*.coffee' ,
+      tasks: ['concat:build/morris.coffee', 'coffee:lib']
     }
   });
 
-  grunt.loadNpmTasks('grunt-coffee');
-  grunt.loadNpmTasks('grunt-mocha');
-  grunt.loadNpmTasks('grunt-contrib-less');
-
-  grunt.registerTask('default', 'concat coffee less min mocha');
+  grunt.registerTask('default', ['concat', 'coffee', 'less', 'uglify', 'mocha']);
 };
